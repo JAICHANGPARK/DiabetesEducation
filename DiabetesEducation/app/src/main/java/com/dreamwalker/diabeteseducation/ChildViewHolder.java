14e1d998cff6f03a2ddb24b74a8ce30ded6b0c42
Dreamwalker <aristojeff@gmail.com>
2018-08-09 00:57:55 +0900

    확장되는 리사이클러뷰 추가

---
+++ DiabetesEducation/app/src/main/java/com/dreamwalker/diabeteseducation/ChildViewHolder.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.dreamwalker.diabeteseducation;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class ChildViewHolder extends com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder {
+
+    private TextView childTextView;
+
+    public ChildViewHolder(View itemView) {
+        super(itemView);
+        childTextView = (TextView) itemView.findViewById(R.id.list_item_artist_name);
+
+    }
+
+    public void setArtistName(String name) {
+        childTextView.setText(name);
+    }
+
+    public void binData(final Child items, final EduAdapter.OnItemClickListener listener){
+        childTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onItemClick(items);
+            }
+        });
+    }
+
+}
--- DiabetesEducation/app/src/main/java/com/dreamwalker/diabeteseducation/MainActivity.java
+++ DiabetesEducation/app/src/main/java/com/dreamwalker/diabeteseducation/MainActivity.java
@@ -1,13 +1,63 @@
 package com.dreamwalker.diabeteseducation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EduAdapter adapter;
+
+    RecyclerView recyclerView;
+
+    ArrayList<String> bigList = new ArrayList<>();
+    ArrayList<String> childList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        RecyclerView.ItemAnimator animator = recyclerView.getItemAnimator();
+        if (animator instanceof DefaultItemAnimator) {
+            ((DefaultItemAnimator) animator).setSupportsChangeAnimations(false);
+        }
+
+        adapter = new EduAdapter(makeGenres());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public static List<Big> makeGenres() {
+
+        return Arrays.asList(makeRockGenre());
+//        return Arrays.asList(makeRockGenre(),
+//                makeJazzGenre(),
+//                makeClassicGenre(),
+//                makeSalsaGenre(),
+//                makeBluegrassGenre());
+    }
+
+    public static Big makeRockGenre() {
+        return new Big("Rock", makeRockArtists());
+    }
+
+    public static List<Child> makeRockArtists() {
+        Child queen = new Child("Queen");
+        Child styx = new Child("Styx");
+        Child reoSpeedwagon = new Child("REO Speedwagon");
+        Child boston = new Child("Boston");
+
+        return Arrays.asList(queen, styx, reoSpeedwagon, boston);
     }
 }
