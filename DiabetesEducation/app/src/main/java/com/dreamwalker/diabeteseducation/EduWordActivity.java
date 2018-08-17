e6d33de669fd2c195f7e751776122bf284e8e35d
yangjihoon <kin7274@naver.com>
2018-08-17 13:30:47 +0900

    인탠트
    인탠트

--- DiabetesEducation/app/src/main/java/com/dreamwalker/diabeteseducation/EduWordActivity.java
+++ DiabetesEducation/app/src/main/java/com/dreamwalker/diabeteseducation/EduWordActivity.java
@@ -1,34 +1,5 @@
 package com.dreamwalker.diabeteseducation;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class EduWordActivity extends AppCompatActivity {
-
-    RecyclerView recycler_view;
-    EduWordAdapter eduWordAdapter;
-    ImageView imageView;
-    ArrayList<Integer> imageList;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edu_word);
-
-        recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
-        recycler_view.setLayoutManager(new LinearLayoutManager(this));  // 한 줄로
-
-        eduWordAdapter = new EduWordAdapter(this, imageList);
-        recycler_view.setAdapter(eduWordAdapter);  // setAdapter();
-    }
+public class EduWordActivity {
+    // 용어 표시 화면
 }
---
+++ Recycler01/app/src/main/java/com/example/administrator/recycler01/FoodInfo.java
@@ -0,0 +1,11 @@
+package com.example.administrator.recycler01;
+
+public class FoodInfo {
+    public int drawableId;
+    public String price;
+
+    public FoodInfo(int drawableId, String price){
+        this.drawableId = drawableId;
+        this.price = price;
+    }
+}
