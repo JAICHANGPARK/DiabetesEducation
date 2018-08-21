13896067b61b3488a5167d76006fee9a6306b0a2
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-08-21 15:08:55 +0900

    메인 형식 및 툴바 이름 수정
    메인 형식 및 툴바 이름 수정

--- app/src/main/java/com/dreamwalker/diabeteseducation/MainActivity.java
+++ app/src/main/java/com/dreamwalker/diabeteseducation/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(mytoolbar);
         getSupportActionBar().setTitle("");
+
+        Button action_word = (Button) findViewById(R.id.action_word);
+        action_word.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 교육(추가자료) 페이지로 이동
+                Intent intent_edu = new Intent(MainActivity.this, EduWordActivity.class);
+                startActivity(intent_edu);
+            }
+        });
+
+        Button action_image = (Button) findViewById(R.id.action_image);
+        action_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 설정 페이지로 이동
+                Intent intent = new Intent(MainActivity.this, EduPdfActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     // 메뉴.xml
@@ -31,19 +53,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        // 툴바 탭 클릭 이벤트
-        switch(item.getItemId()){
-            case R.id.action_image:
-                // 설정 페이지로 이동
-                Intent intent = new Intent(MainActivity.this, EduPdfActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.action_word:
-                // 교육(추가자료) 페이지로 이동
-                Intent intent_edu = new Intent(MainActivity.this, EduWordActivity.class);
-                startActivity(intent_edu);
-                break;
-        }
+//        // 툴바 탭 클릭 이벤트
+//        switch(item.getItemId()){
+//            case R.id.action_word:
+//                break;
+//        }
         return super.onOptionsItemSelected(item);
     }
 }
