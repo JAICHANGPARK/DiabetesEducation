949fe883749a0ff7356d74872db04f376d563364
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-07 14:14:07 +0900

    트레비스아저씨..
    트레비스아저씨..

--- app/src/main/java/com/dreamwalker/diabeteseducation/introduction/IntroActivity.java
+++ app/src/main/java/com/dreamwalker/diabeteseducation/introduction/IntroActivity.java
@@ -26,9 +26,7 @@ public class IntroActivity extends AppCompatActivity implements AdapterView.OnIt
         setContentView(R.layout.activity_intro);
         setStatusbar();
         set_scroll();
-
         TextView tvLinkify = (TextView) findViewById(R.id.tvLinkify);
-
         TextView abc = (TextView) findViewById(R.id.abc);
         elv = (ExpandableListView) findViewById(R.id.elv);
         final ArrayList<Index> index = getData();
