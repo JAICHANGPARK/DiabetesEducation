f23bb51d46b3ba2b46428543bf71e259291b86bd
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-07 16:40:40 +0900

    영상 페이지 상태바  색 변경
    영상 페이지 상태바  색 변경

--- app/src/main/java/com/dreamwalker/diabeteseducation/activity/EduYoutueActivity.java
+++ app/src/main/java/com/dreamwalker/diabeteseducation/activity/EduYoutueActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.dreamwalker.diabeteseducation.R;
 
@@ -13,7 +15,17 @@ public class EduYoutueActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.youtube);
+        setStatusbar();
         setToolbar();
+        
+    }
+
+    // 상태바 색 변경
+    public void setStatusbar(){
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryPurle));
     }
 
     // 툴바
