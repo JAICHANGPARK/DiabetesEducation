5342342890488da596e4c46d8950b50586223a38
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-07 16:37:48 +0900

    영상페이지 앱바 생성
    영상페이지 앱바 생성

--- app/src/main/java/com/dreamwalker/diabeteseducation/activity/EduYoutueActivity.java
+++ app/src/main/java/com/dreamwalker/diabeteseducation/activity/EduYoutueActivity.java
@@ -3,6 +3,7 @@ package com.dreamwalker.diabeteseducation.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.dreamwalker.diabeteseducation.R;
 
@@ -12,6 +13,13 @@ public class EduYoutueActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.youtube);
+        setToolbar();
+    }
 
+    // 툴바
+    public void setToolbar() {
+        Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mytoolbar);
+        getSupportActionBar().setTitle("");
     }
 }
